d09165b317f6e3eb2c860327ebe6527d3aa08826
student <student@B206-7>
2023-03-07 11:25:39 +0700

    Добавьте файлы проекта.

---
+++ patarn/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace patarn
+{
+    public partial class Form1 : Form
+    {
+        List<Itest> tests = new List<Itest>()
+            {
+                new normtest(),
+                new difftest(),
+                new exam()
+            };
+        List<Student> students = new List<Student>();
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(new string[3] {tests[0].Name, tests[1].Name, tests[2].Name});
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            students.Add(new Student(textBox1.Text, tests[comboBox1.SelectedIndex]));
+            listBox1.Items.Add($"Студент: {students[listBox1.Items.Count].Name}, Экзамен: {students[listBox1.Items.Count].test.Name}");
+        }
+    }
+}
---
+++ patarn/zach.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patarn
+{
+    interface Itest
+    {
+        string Name { get; }
+        int Grade { get; set; }
+        bool Passed { get; set; }
+    }
+
+    class difftest : Itest
+    {
+        
+        private int grade;
+        private bool passed;
+        private string name = "дифференциальный зачет";
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public int Grade
+        {
+            get
+            {
+                return grade;
+            }
+            set
+            {
+                if (value >= 1 && value <= 5)
+                    grade = value;
+                Passed = true;
+            }
+        }
+
+        public bool Passed
+        {
+            get
+            {
+                return passed;
+            }
+            set
+            {
+                if (grade >= 3)
+                    passed = true;
+            }
+        }
+
+    }
+
+    class normtest : Itest
+    {
+        public int Grade { get; set; }
+        private bool passed;
+
+        private string name = "зачет";
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public bool Passed
+        {
+            get
+            {
+                return passed;
+            }
+            set
+            {
+                passed = value;
+            }
+        }
+    }
+
+    class exam : Itest
+    {
+        private int grade;
+        private bool passed;
+        private string name = "экзамен";
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public int Grade
+        {
+            get
+            {
+                return grade;
+            }
+            set
+            {
+                if (value >= 1 && value <= 100)
+                    grade = value;
+                Passed = true;
+            }
+        }
+
+        public bool Passed
+        {
+            get
+            {
+                return passed;
+            }
+            set
+            {
+                if (grade >= 70)
+                    passed = true;
+            }
+        }
+    }
+    class Student
+    {
+        public string Name { get; set; }
+        public Itest test;
+
+        public Student(string name, Itest tes)
+        {
+            Name = name;
+            test = tes;
+        }
+    }
+}
